8fd80b7f3512ad9f21c16f2e963da46b7caa88ec
GloireMutaliko21 <mufunyig@gmail.com>
2024-06-04 15:28:58 +0200

    register and login with adapter

--- auth.ts
+++ auth.ts
@@ -1,48 +1,20 @@
 import NextAuth from "next-auth";
-import { TypeORMAdapter } from "@auth/typeorm-adapter";
+import {
+  AdapterAccount,
+  AdapterSession,
+  AdapterUser,
+  type Adapter,
+} from "@auth/core/adapters";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-import {
-  UserEntity,
-  AccountEntity,
-  SessionEntity,
-  VerificationTokenEntity,
-} from "@/libs/entities";
-import { DataSource } from "typeorm";
-
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DATABASE_HOST,
-  port: 5432,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: [UserEntity, AccountEntity, SessionEntity, VerificationTokenEntity],
-  synchronize: true,
-  logging: false,
-});
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-  });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: CustomAdapter(),
   providers: [
     Google({ allowDangerousEmailAccountLinking: true }),
     Github({ allowDangerousEmailAccountLinking: true }),
   ],
-  adapter: TypeORMAdapter(process.env.AUTH_TYPEORM_CONNECTION ?? "", {
-    entities: {
-      AccountEntity,
-      SessionEntity,
-      UserEntity,
-      VerificationTokenEntity,
-    },
-  }),
+
   callbacks: {
     async signIn({ account }) {
       if (account) {
@@ -51,5 +23,73 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return false;
     },
   },
-  debug: true,
+  // debug: true,
 });
+
+export function CustomAdapter(): Adapter {
+  return {
+    createUser: async (user): Promise<any> => {
+      const response = await fetch("http://api_Url:3080/user", {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: user.name,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          image: user.image
+        }),
+        method: "POST",
+      });
+      const createdUser = await response.json();
+      return createdUser as AdapterUser;
+    },
+
+    getUser: async (id): Promise<any> => {
+      return null;
+    },
+
+    getUserByEmail: async (email): Promise<any> => {
+      return null;
+    },
+
+    getUserByAccount: async (
+      providerAccountId: Pick<AdapterAccount, "provider" | "providerAccountId">
+    ): Promise<any> => {
+      return null;
+    },
+
+    updateUser: async (
+      user: Partial<AdapterUser> & Pick<AdapterUser, "id">
+    ): Promise<any> => {
+      return user;
+    },
+
+    linkAccount: async (account: AdapterAccount) => {
+      return account;
+    },
+
+    createSession: async (session: {
+      sessionToken: string;
+      userId: string;
+      expires: Date;
+    }) => {
+      return session;
+    },
+
+    getSessionAndUser: async (sessionToken: string): Promise<any> => {
+      return sessionToken;
+    },
+
+    updateSession: async (
+      session: Partial<AdapterSession> & Pick<AdapterSession, "sessionToken">
+    ): Promise<any> => {
+      return session;
+    },
+
+    deleteSession: async (sessionToken: string): Promise<any> => {
+      return sessionToken;
+    },
+  };
+}
+
